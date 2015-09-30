c20a08fdd8fb153311099cbc3241ae10b8cda3ae
Louis Martin-Pierrat <mart_p@localhost.localdomain>
2015-09-30 23:11:27 +0200

    Default Project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TerrainRendering
 Utilisation d'OpenGL 4+ avec Qt pour faire un rendering de terrain.
+
+# Compilation
+mkdir cmake
+cd cmake
+cmake -DCMAKE_BUILD_TYPE=release ..
+make
---
+++ sources/main.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file main.cpp
+/// \brief Terrain Rendering Demo program
+/// \author Louis Martin-Pierrat (mart_p)
+/// \version 1.0.0
+/// \date September 30, 2015
+////////////////////////////////////////////////////////////////////////////////
+
+#include "terrain-rendering.hpp"
+
+#include <iostream>
+
+int     main(void)
+{
+    std::cout << "TerrainRendering version : "
+        << TERRAIN_RENDERING_VERSION_FULL
+        << std::endl;
+    return 0;
+}
