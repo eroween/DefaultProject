2d1faf39e76b2aa1029d644507da6988c23c70af
eroween <louismartinpierrat@gmail.com>
2016-01-06 16:12:13 -0800

    Default Project configuration update, doxygen generation into doc folder, renaming from old use

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,19 +1,21 @@
-////////////////////////////////////////////////////////////////////////////////
+///
 /// \file main.cpp
-/// \brief Terrain Rendering Demo program
+///
+/// \brief Default project main
+///
 /// \author Louis Martin-Pierrat (mart_p)
+///
 /// \version 1.0.0
+///
 /// \date September 30, 2015
-////////////////////////////////////////////////////////////////////////////////
-
-#include "terrain-rendering.hpp"
+///
 
 #include <iostream>
 
+#include "project.hpp"
+
 int     main(void)
 {
-    std::cout << "TerrainRendering version : "
-        << TERRAIN_RENDERING_VERSION_FULL
-        << std::endl;
+    std::cout << "Project version : " << PROJECT_VERSION_FULL << std::endl;
     return 0;
 }
