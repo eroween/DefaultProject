f1b984944e73c0c82134e16ff0d743a0f416abd4
eroween <louismartinpierrat@gmail.com>
2016-04-02 15:01:55 -0700

    put /Mt & /MTd only for MSVC

--- cmake/compilation_settings.cmake
+++ cmake/compilation_settings.cmake
@@ -15,33 +15,6 @@
 ## Copyright (C) 2016 Martin-Pierrat Louis (louismartinpierrat@gmail.com)
 ##
 
-macro(fix_default_compiler_settings_)
-
-    IF (MSVC)
-        # For MSVC, CMake sets certain flags to defaults we want to override.
-        # This replacement code is taken from sample in the CMake Wiki at
-        # http://www.cmake.org/Wiki/CMake_FAQ#Dynamic_Replace.
-        FOREACH (flag_var
-                CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-                CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-            IF (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
-                # When Google Test is built as a shared library, it should also use
-                # shared runtime libraries.  Otherwise, it may end up with multiple
-                # copies of runtime library data in different modules, resulting in
-                # hard-to-find crashes. When it is built as a static library, it is
-                # preferable to use CRT as static libraries, as we don't have to rely
-                # on CRT DLLs being available. CMake always defaults to using shared
-                # CRT libraries, so we override that default here.
-                STRING(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
-            ENDIF ()
-
-            # We prefer more strict warning checking for building Google Test.
-            # Replaces /W3 with /W4 in defaults.
-            string(REPLACE "/W3" "/W4" ${flag_var} "${${flag_var}}")
-        endforeach()
-    endif()
-endmacro()
-
 FUNCTION(CONFIGURE_COMPILATION)
 
     IF (CMAKE_COMPILER_IS_GNUCXX)
@@ -99,7 +72,5 @@ FUNCTION(CONFIGURE_COMPILATION)
         TARGET_COMPILE_OPTIONS(${CMAKE_PROJECT_NAME} PRIVATE -std=c++14)
 
     ENDIF()
-
-    fix_default_compiler_settings_()
 
 ENDFUNCTION()
